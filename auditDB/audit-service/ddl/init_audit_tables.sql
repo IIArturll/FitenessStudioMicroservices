91b494d8b56219dfd137bb9c168e1e44a9bcb08c
Artur <sharam.artur@gmail.com>
2023-03-19 23:04:40 +0300

    config audit

--- auditDB/audit-service/ddl/init_audit_tables.sql
+++ auditDB/audit-service/ddl/init_audit_tables.sql
@@ -1,14 +1,14 @@
 CREATE SCHEMA IF NOT EXISTS fitness;
-CREATE TABLE fitness.user
+CREATE TABLE fitness.audit
 (
     uuid UUID NOT NULL,
     dt_create TIMESTAMP(6) WITH TIME ZONE NOT NULL,
-    user_uuid UUID NIT NULL,
-    mail TEXT NOT NULL UNIQUE,
+    user_uuid UUID NOT NULL,
+    mail TEXT NOT NULL,
     fio TEXT NOT NULL,
     role TEXT NOT NULL,
     text TEXT NOT NULL,
     type TEXT NOT NULL,
     id TEXT NOT NULL,
-    CONSTRAINT uuid_user PRIMARY KEY (id)
+    CONSTRAINT audit_pk PRIMARY KEY (uuid)
 );
